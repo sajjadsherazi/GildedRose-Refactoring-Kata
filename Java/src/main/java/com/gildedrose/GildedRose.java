3aa76f45eb167352aa1f4a2b4b2fb0eaaf0fd1d2
ssherazi <sajjad.sherazi@nokia.com>
2023-08-14 19:32:06 -0400

    Gilded Rose: Conjured Items feature

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -12,8 +12,15 @@ class GildedRose {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros") &&
+                            !items[i].name.equals("Conjured Mana Cake")) {
                         items[i].quality = items[i].quality - 1;
+                    } else if (items[i].name.equals("Conjured Mana Cake")) {
+                        if (items[i].quality > 1) {
+                            items[i].quality = items[i].quality - 2;
+                        } else {
+                            items[i].quality = 0;
+                        }
                     }
                 }
             } else {
@@ -44,8 +51,15 @@ class GildedRose {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros") &&
+                                !items[i].name.equals("Conjured Mana Cake")) {
                                 items[i].quality = items[i].quality - 1;
+                            } else if (items[i].name.equals("Conjured Mana Cake")) {
+                                if (items[i].quality > 1) {
+                                    items[i].quality = items[i].quality - 2;
+                                } else {
+                                    items[i].quality = 0;
+                                }
                             }
                         }
                     } else {
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -11,7 +11,56 @@ class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
+    }
+
+    /**
+     * Test to assert that Conjured items degrade in Quality twice as fast as normal items
+     */
+    @Test
+    void conjuredItemQualityDegradingTwiceFast() {
+        int sellIn = 6;
+        int quality = 21;
+        Item[] items = new Item[]{
+            new Item("Conjured Mana Cake", sellIn, quality)
+        };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(quality - 2, app.items[0].quality);
+        assertEquals(sellIn - 1, app.items[0].sellIn);
+    }
+
+    /**
+     * Test to assert that Conjured items degrade in Quality twice as fast as normal items after
+     * the sellIn date
+     */
+    @Test
+    void conjuredItemQualityDegradingTwiceFastAfterSellInDate() {
+        int sellIn = 0;
+        int quality = 21;
+        Item[] items = new Item[]{
+            new Item("Conjured Mana Cake", sellIn, quality)
+        };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(quality - 4, app.items[0].quality);
+        assertEquals(sellIn - 1, app.items[0].sellIn);
+    }
+
+    /**
+     * Test to assert that Conjured items quality cannot be negative
+     */
+    @Test
+    void conjuredItemQualityCannotBeNegative() {
+        int sellIn = 6;
+        int quality = 1;
+        Item[] items = new Item[]{
+            new Item("Conjured Mana Cake", sellIn, quality)
+        };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+        assertEquals(sellIn - 1, app.items[0].sellIn);
     }
 
 }
